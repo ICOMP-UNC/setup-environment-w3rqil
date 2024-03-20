2a331bcba163d8554d2601e26a21c80071bcab14
w3rqil <leonel.mansilla@mi.unc.edu.ar>
2024-03-20 11:27:18 -0300

    -add executable.

---
+++ Desktop/SOII/LAB0SETUP/src/main.c
@@ -0,0 +1,15 @@
+#include <greetings.h>
+#include <stdio.h>
+
+    int main()
+{
+    printf("---------------------------------------------\n");
+    printf(" Hello, World! - OSII Lab0 Enviroment setup.\n");
+    printf("---------------------------------------------\n\n");
+
+    // Call the function from the include library
+    greetings();
+    printf("\n");
+
+    return 0;
+}

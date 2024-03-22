d478a0ff257baf4df3b65074d457407fcfefb08e
w3rqil <leonel.mansilla@mi.unc.edu.ar>
2024-03-22 10:36:19 -0300

    prueba

---
+++ Desktop/SOII/LAB0SETUP/build/lib/libsta/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leonel/Desktop/SOII/LAB0SETUP/lib/libsta
+# Build directory: /home/leonel/Desktop/SOII/LAB0SETUP/build/lib/libsta
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
